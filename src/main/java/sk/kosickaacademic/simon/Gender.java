cb42d4fd0a6e391f764e91c213a7cb6257a022d3
Simon Senic <simon.senic@kosickaakademia.sk>
2021-03-02 10:39:55 +0100

    Added new class Gender, updated class User

---
+++ src/main/java/sk/kosickaacademic/simon/Gender.java
@@ -0,0 +1,5 @@
+package sk.kosickaacademic.simon;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
--- src/main/java/sk/kosickaacademic/simon/entity/User.java
+++ src/main/java/sk/kosickaacademic/simon/entity/User.java
@@ -1,22 +1,41 @@
 package sk.kosickaacademic.simon.entity;
 
+import sk.kosickaacademic.simon.Gender;
+
 public class User {
     private int id, age;
     private String fName, lName;
-    private boolean gender;
+    private Gender gender;
 
-    public User(int id, int age, String fName, String lName, boolean gender) {
+    public User(int id, int age, String fName, String lName, int gender) {
+        this(age, fName, lName, gender);
         this.id = id;
-        this.age = age;
-        this.fName = fName;
-        this.lName = lName;
-        this.gender = gender;
     }
 
-    public User(int age, String fName, String lName, boolean gender) {
+    public User(int age, String fName, String lName, int gender) {
         this.age = age;
         this.fName = fName;
         this.lName = lName;
-        this.gender = gender;
+        this.gender = gender==0?Gender.MALE:gender==1?Gender.FEMALE:Gender.OTHER;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return fName;
+    }
+
+    public String getLastName() {
+        return lName;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
