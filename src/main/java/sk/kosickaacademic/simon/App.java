72e1bc96f418e8d284711e6fdb09709c79a09323
Simon Senic <simon.senic@kosickaakademia.sk>
2021-03-03 11:00:48 +0100

    Added new method insertNewUser(), updated class Gender

--- src/main/java/sk/kosickaacademic/simon/App.java
+++ src/main/java/sk/kosickaacademic/simon/App.java
@@ -1,12 +1,13 @@
 package sk.kosickaacademic.simon;
 
 import sk.kosickaacademic.simon.database.Database;
+import sk.kosickaacademic.simon.entity.User;
 
 public class App 
 {
     public static void main( String[] args )
     {
         Database db = new Database();
-        db.getConnection();
+        //db.insertNewUser(new User(98, "Meky", "Zbirka", 0));
     }
 }
--- src/main/java/sk/kosickaacademic/simon/database/Database.java
+++ src/main/java/sk/kosickaacademic/simon/database/Database.java
@@ -1,10 +1,12 @@
 package sk.kosickaacademic.simon.database;
 
 import sk.kosickaacademic.simon.Log;
+import sk.kosickaacademic.simon.entity.User;
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -34,5 +36,28 @@ public class Database {
         }catch (SQLException e){
             log.printError(e.toString());
         }
+    }
+
+    public boolean insertNewUser(User user){
+        String query = "INSERT INTO user (fname, lname, age, gender) VALUES(?, ?, ?, ?)";
+        try{
+            Connection con = getConnection();
+            if(con!=null){
+                PreparedStatement ps = con.prepareStatement(query);
+                ps.setString(1, user.getFirstName());
+                ps.setString(2, user.getLastName());
+                ps.setInt(3, user.getAge());
+                ps.setInt(4, user.getGender().getValue());
+                int result = ps.executeUpdate();
+                if(result==1){
+                    log.printMessage("User successfully added");
+                    return true;
+                }
+            }
+            con.close();
+        }catch (SQLException e){
+            log.printError(e.toString());
+        }
+        return false;
     }
 }
